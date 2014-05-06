94e0478013cefd986a3b708bfde492ba61df83ee
Joe Wooster <jeebobs@btinternet.com>
2014-05-06 09:49:13 +0100

    Revert "fusion3: exFAT support"
    This reverts commit 1725c49d6ac2995b12f665884a78245c2e230bb2.
    
    Change-Id: Iadbfc3356c6e8440f6a90b43e1e850202539f4a4

--- fusion3.mk
+++ fusion3.mk
@@ -150,10 +150,7 @@ PRODUCT_PACKAGES += \
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-    e2fsck \
-    mount.exfat \
-    fsck.exfat \
-    mkfs.exfat
+    e2fsck
 
 # We have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
